a4ae685acf0b4199d1f7d29f8867c275cb20dcf6
maximimby <maximimby@gmail.com>
2021-10-12 23:48:44 +0300

    changed implementaion plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -17,16 +17,36 @@ const convertKey = (key, prefix) => [...prefix, key].join('.');
 const plain = (differences, prefix = []) => {
   const array = [];
 
-  differences.forEach(({
+  const convertedDifferences = [];
+
+  for (let i = 0; i < differences.length; i += 1) {
+    const diff = differences[i];
+    const index = i;
+    if (diff.type === 'REMOVED' && differences[index + 1]?.type === 'ADDED' && differences[index + 1]?.key === diff.key) {
+      convertedDifferences.push({
+        type: 'UPDATED',
+        key: diff.key,
+        value1: diff.value,
+        value2: differences[index + 1].value,
+      });
+      i += 1;
+    } else {
+      convertedDifferences.push(diff);
+    }
+  }
+
+  convertedDifferences.forEach(({
     type,
     key,
     value,
+    value1,
+    value2,
     differences: nestedDifferences,
-  }, index) => {
+  }) => {
     if (type === 'NESTED') {
       array.push(plain(nestedDifferences, [...prefix, key]));
-    } else if (type === 'REMOVED' && differences[index + 1]?.type === 'ADDED' && differences[index + 1]?.key === key) {
-      array.push(`Property '${convertKey(key, prefix)}' was updated. From ${convertValue(value)} to ${convertValue(differences[index + 1].value)}`);
+    } else if (type === 'UPDATED') {
+      array.push(`Property '${convertKey(key, prefix)}' was updated. From ${convertValue(value1)} to ${convertValue(value2)}`);
     } else if (type === 'REMOVED') {
       array.push(`Property '${convertKey(key, prefix)}' was removed`);
     } else if (type === 'ADDED') {
